dae93700be3346a3196ee8c6f57816928f458982
ryonakao <rodriguez.nak@gmail.com>
2017-09-05 12:58:37 +0900

    Add processing dial

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
-func exists(domain string) (bool, error) {
+import "net"
 
+func exists(domain string) (bool, error) {
+	const whoisServer string = "com.whois-servers.net"
+	conn, err := net.Dial("tcp", whoisServer+":43")
 }
