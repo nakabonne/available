b8e0e8fb8e92b17589be0278659069d252f7d118
ryonakao <rodriguez.nak@gmail.com>
2017-09-05 13:03:51 +0900

    complete to define exists

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"net"
+	"strings"
+)
 
 func exists(domain string) (bool, error) {
 	const whoisServer string = "com.whois-servers.net"
 	conn, err := net.Dial("tcp", whoisServer+":43")
+	if err != nil {
+		return false, err
+	}
+	defer conn.Close()
+	conn.Write([]byte(domain + "\r\n"))
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		if strings.Contains(strings.ToLower(scanner.Text()), "no match") {
+			return false, nil
+		}
+	}
+	return true, nil
 }
